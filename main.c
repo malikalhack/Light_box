91a826661e225b4f7ea71cf26388e6291d4b76d7
Anton Chernov <malikalhack@gmail.com>
2022-11-22 03:39:34 +0200

    Replaced tabs with spaces Updated standard.h file

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
-/************************************
-* Title	 	: main.c				*
-* Release	: 0.1.B					*
-* Creator	: Malik					*
-* Created	: 25.01.2016 17:00:24	*
-* Changed	: 21.10.2020			*
-* MCU	 	: ATtiny13A		 		*
-* Frequency	: 1.2 MHz (int)			*
-************************************/
+/**
+ * @file    main.c
+ * @version 1.0.0
+ * @authors Anton Chernov
+ * @date    25.01.2016 17:00:24
+ * @date    21.10.2020
+ * @note    MCU:   ATtiny13A
+ * @note    Clock: 1.2 MHz
+ */
 
 /******************************** Included files **********************************/
 #include "standard.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 /********************************** Definition ************************************/
 #define DISCRETELY
-#define MEAS_NUM	10
+#define MEAS_NUM    10
 #define DEF_COUNTER 18
 /****************************** Private prototypes ********************************/
 ISR (ADC_vect);
@@ -26,73 +28,73 @@ static BYTE step = 0;
 static BYTE counter = DEF_COUNTER;
 static BYTE mesurment[MEAS_NUM] = {};
 /******************************* Additional Definition ****************************/
-#define CHK_FLAG	control&BIT(0)
-#define SET_FLAG	control |= BIT(0)
-#define CLR_FLAG	control &= ~BIT(0)
+#define CHK_FLAG    control&BIT(0)
+#define SET_FLAG    control |= BIT(0)
+#define CLR_FLAG    control &= ~BIT(0)
 /********************************** Entry point ***********************************/
 int main(void) {
     init();
 /********************************* Endlessly loop *********************************/
     while (1) {
-		if(CHK_FLAG) {
-			CLR_FLAG;
-			adjust(calc());
-		}
+        if(CHK_FLAG) {
+            CLR_FLAG;
+            adjust(calc());
+        }
     }
 /**********************************************************************************/
 }
 
 /******************************** Private functions *******************************/
 void init (void) {
-	DDRB	=  0x0f;
-	PORTB	=  0;
-	OCR0A	=  1;
-	TCCR0A	=  0x83;				/* Fast-PWM, non-invert mode */
-	TCCR0B	=  0x03;				/* No pre-scaling (clock/64) */
-	TIMSK0  =  0x02;
-	ADMUX	=  0x22;				/* ADC2(PB4), reference - Vcc, left adjust */
-	ADCSRA	=  0x8b;				/* ADC and interrupt are enable, div8 */
-	ADCSRA |=  BIT(ADSC);			/* start ADC */
-	sei();
+    DDRB    =  0x0f;
+    PORTB   =  0;
+    OCR0A   =  1;
+    TCCR0A  =  0x83;                /* Fast-PWM, non-invert mode */
+    TCCR0B  =  0x03;                /* No pre-scaling (clock/64) */
+    TIMSK0  =  0x02;
+    ADMUX   =  0x22;                /* ADC2(PB4), reference - Vcc, left adjust */
+    ADCSRA  =  0x8b;                /* ADC and interrupt are enable, div8 */
+    ADCSRA |=  BIT(ADSC);           /* start ADC */
+    sei();
 }
 /**********************************************************************************/
 BYTE calc (void) {
-	cli();
-	register BYTE i, min;
-	min = mesurment[0];
-	for (i = 1; i < MEAS_NUM; i++) {
-		if (min > mesurment[i]) min = mesurment[i];
-	}
-	sei();
-	return min;
+    cli();
+    register BYTE i, min;
+    min = mesurment[0];
+    for (i = 1; i < MEAS_NUM; i++) {
+        if (min > mesurment[i]) min = mesurment[i];
+    }
+    sei();
+    return min;
 }
 /**********************************************************************************/
 void adjust (register BYTE result) {
-	register BYTE t_OCR0A = OCR0A;
-	if (result == t_OCR0A) return;
-#ifdef DISCRETELY
-	OCR0A = result;
-#else
-	while (result != t_OCR0A) {
-		if (result > t_OCR0A) t_OCR0A++;
-		else t_OCR0A--;
-		OCR0A = t_OCR0A;
-	}
-#endif
+    register BYTE t_OCR0A = OCR0A;
+    if (result == t_OCR0A) return;
+    #ifdef DISCRETELY
+    OCR0A = result;
+    #else
+    while (result != t_OCR0A) {
+        if (result > t_OCR0A) t_OCR0A++;
+        else t_OCR0A--;
+        OCR0A = t_OCR0A;
+    }
+    #endif
 }
 /**********************************************************************************/
 ISR (ADC_vect) {
-	mesurment[step] = ADCH;			/* without the last two bits */
-	if (++step == MEAS_NUM) {
-		step = 0;
-		SET_FLAG;
-	}
+    mesurment[step] = ADCH;         /* without the last two bits */
+    if (++step == MEAS_NUM) {
+        step = 0;
+        SET_FLAG;
+    }
 }
 /**********************************************************************************/
 ISR (TIM0_OVF_vect) {
-	if (!(--counter)) {
-		counter = DEF_COUNTER;
-		ADCSRA |=  BIT(ADSC);
-	}
+    if (!(--counter)) {
+        counter = DEF_COUNTER;
+        ADCSRA |=  BIT(ADSC);
+    }
 }
 /**********************************************************************************/
--- standard.h
+++ standard.h
@@ -1,72 +1,61 @@
-/********************************
-* Title   : Standard.h			*
-* Release : 1.1					*
-* Creator : Malik				*
-* Platform: AVR					*
-* Created : 15.05.2017 14:17:28	*
-* Changed : 20.10.2020			*
-********************************/
+/**
+ * @file    Standard.h
+ * @version 1.5.0
+ * @authors Anton Chernov
+ * @date    15.05.2017
+ * @date    14.05.2022
+ */
 
 #ifndef STANDARD_H
 #define STANDARD_H
-/********************************************************************************/
-#include <avr/io.h>
-#include <avr/interrupt.h>
-/********************************* Definition ***********************************/
-#define BIT(bit) (1 << (bit))
-#define NULLPTR  ((void*)0)
-#define FALSE	 0
-#define TRUE	 1
-/********************************************************************************/
-typedef unsigned char 		BYTE;
-typedef signed char 		SBYTE;
-typedef unsigned int 		WORD;
-typedef signed int 			SWORD;
-typedef unsigned long int	DWORD;
-typedef signed long int 	SDWORD;
-typedef unsigned long long 	QWORD;
-typedef signed long long 	SQWORD; 
-
-typedef unsigned char 		BOOLEAN; /*  8 bits - use TRUE or FALSE for this type */
-typedef char 				CHAR;    /*  For use in character arrays. Use int type otherwise. */
-typedef unsigned char 		UINT8;   /*  8 bits */
-typedef signed char 		SINT8;   /*  8 bits */
-typedef unsigned short 		UINT16;  /* 16 bits */
-typedef signed short 		SINT16;  /* 16 bits */
-typedef unsigned int 		UINT32;  /* 32 bits */
-typedef signed int 			SINT32;  /* 32 bits */
-typedef unsigned long long 	UINT64;  /* 64 bits */
-typedef signed long long 	SINT64;  /* 64 bits */
-typedef float 				FLOAT;   /* 32 bits */
-typedef float 				FLOAT32; /* 32 bits */
-typedef double 				DOUBLE;  /* 64 bits */
-typedef double 				FLOAT64; /* 64 bits */
-
-#define BIT7 ((UINT8)0x80)
-#define BIT6 ((UINT8)0x40)
-#define BIT5 ((UINT8)0x20)
-#define BIT4 ((UINT8)0x10)
-#define BIT3 ((UINT8)0x08)
-#define BIT2 ((UINT8)0x04)
-#define BIT1 ((UINT8)0x02)
-#define BIT0 ((UINT8)0x01)
+/******************************* Definition ***********************************/
+#define BIT(bit)            (1 << (bit))
+#define NULLPTR             ((void*)0)
+#define UNUSED_PARAM(param) ((void)param)
+#define DISCARD_RETURN(func)(UNUSED_PARAM(func))
+#define NORETURN            __declspec(noreturn) void
+#define FALSE               (0)
+#define TRUE                (1)
+/******************************************************************************/
+typedef unsigned char       BYTE;   ///<  8-bit unsigned integer.
+typedef signed char         SBYTE;  ///<  8-bit signed integer.
+typedef unsigned int        WORD;   ///< 16 bit unsigned integer.
+typedef signed int          SWORD;  ///< 16 bit signed integer.
+typedef unsigned long int   DWORD;  ///< 32 bit unsigned integer.
+typedef signed long int     SDWORD; ///< 32 bit signed integer.
+typedef unsigned long long  QWORD;  ///< 64 bit unsigned integer.
+typedef signed long long    SQWORD; ///< 64 bit signed integer.
+/******************************************************************************/
+typedef unsigned char       BOOLEAN; ///<  8 bits - use TRUE or FALSE for this
+typedef char                CHAR;    ///<  For use in character arrays only.
+typedef unsigned char       UINT8;   ///<  8-bit unsigned integer.
+typedef signed char         SINT8;   ///<  8-bit signed integer.
+typedef unsigned short      UINT16;  ///< 16 bit unsigned integer.
+typedef signed short        SINT16;  ///< 16 bit signed integer.
+typedef unsigned int        UINT32;  ///< 32 bit unsigned integer.
+typedef signed int          SINT32;  ///< 32 bit signed integer.
+typedef unsigned long long  UINT64;  ///< 64 bit unsigned integer.
+typedef signed long long    SINT64;  ///< 64 bit signed integer.
+typedef float               FLOAT;   ///< 32 bit
+typedef float               FLOAT32; ///< 32 bit
+typedef double              DOUBLE;  ///< 64 bit
+typedef double              FLOAT64; ///< 64 bit
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 /************************ Something like a <limits.h> ***************************/
-#define SBYTE_MIN	-128
-#define SWORD_MIN	-32768
-#define SDWORD_MIN	-2147483648
-#define SQWORD_MIN	-9223372036854775808
-#define SBYTE_MAX	127
-#define SWORD_MAX	32767
-#define SDWORD_MAX	2147483647
-#define SQWORD_MAX	9223372036854775807
-#define BYTE_MAX	255
-#define WORD_MAX	65535
-#define DWORD_MAX	4294967295
-#define QWORD_MAX	18446744073709551615
-
+#define SBYTE_MIN   -128
+#define SWORD_MIN   -32768
+#define SDWORD_MIN  -2147483648
+#define SQWORD_MIN  -9223372036854775808
+#define SBYTE_MAX   127
+#define SWORD_MAX   32767
+#define SDWORD_MAX  2147483647
+#define SQWORD_MAX  9223372036854775807
+#define BYTE_MAX    255
+#define WORD_MAX    65535
+#define DWORD_MAX   4294967295
+#define QWORD_MAX   18446744073709551615
 /********************************************************************************/
 #endif /* STANDARD_H */
